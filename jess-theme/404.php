d7a22acf75de87f81620a25b399e58744654e316
Jess81 <jess.mmstudio@gmail.com>
2020-01-31 11:44:54 -0800

    Updated 404 layout

--- jess-theme/404.php
+++ jess-theme/404.php
@@ -1,13 +1,9 @@
 <?php get_header(); ?>
 <div id="content">
-
-<div>&nbsp;</div>
+<div class="wrapper clearfix">
 <h2>404 Not Found</h2>
 <div>&nbsp;</div>
 <a href="<?php echo get_settings('home'); ?>">Return To Home Page</a>
-<div>&nbsp;</div>
-<div>&nbsp;</div>
-<div>&nbsp;</div>
 
 </div>
 </div>
